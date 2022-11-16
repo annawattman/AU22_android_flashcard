2f4729e50a5626f16ac9eec3e63d92e9353ddf2e
annawattman <annalinneahs@hotmail.com>
2022-11-16 15:55:14 +0100

    new

--- app/src/main/java/com/example/au22_flashcard/MainActivity.kt
+++ app/src/main/java/com/example/au22_flashcard/MainActivity.kt
@@ -1,23 +1,22 @@
 package com.example.au22_flashcard
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.MotionEvent
 import android.widget.Button
 import android.widget.TextView
-import androidx.room.Room
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.*
+import java.util.*
 import kotlin.coroutines.CoroutineContext
 
 class MainActivity : AppCompatActivity(), CoroutineScope {
 
-    lateinit var wordView : TextView
-    var currentWord : Word? = null
-    val wordList = WordList()
+    lateinit var wordView: TextView
+    var currentWord: Word? = null
+    var wordList = mutableListOf<Word>()
     private lateinit var job: Job
-    private lateinit var db : AppDatabase
+    private lateinit var db: AppDatabase
 
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.Main + job
@@ -29,45 +28,20 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
         job = Job()
 
         /*------------- ---------------- */
-        db = Room.databaseBuilder(applicationContext,
-            AppDatabase::class.java,
-            "word-items").fallbackToDestructiveMigration().build()
-
-
-
-        val word1 = Word(0, "Hello", "Hej")
-        val word2 = Word(0, "Good bye", "Hej då")
-        val word3 = Word(0, "Thank you", "Tack")
-        val word4 = Word(0, "Welcome", "Välkommen")
-        val word5 = Word(0, "Computer", "Dator")
-
-     /*    saveWord(word1)
-        saveWord(word2)
-        saveWord(word3)
-        saveWord(word4)
-        saveWord(word5) */
-
-        val list = loadAllWords()
-
-        launch{
-            val wordList = list.await()
-
-            for (word in wordList) {
-                Log.d("!!!", "word: $word")
-            }
-        }
-
-
         db = AppDatabase.getInstance(this)
 
         wordView = findViewById(R.id.wordTextView)
-
-        showNewWord()
 
         wordView.setOnClickListener {
             revealTranslation()
         }
 
+        val list = loadAllWords()
+
+        launch {
+            wordList = list.await() as MutableList<Word>
+            showNewWord()
+        }
 
         /* ---------ADD WORDS BUTTON --------- */
         val addWordsBtn = findViewById<Button>(R.id.addWordsBtn) // Lägger till min knapp
@@ -77,13 +51,27 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
         }
     }
 
-    fun saveWord(word : Word) {
-        launch(Dispatchers.IO) {
-            db.wordDao().insert(word)
+
+    fun getNewWord(): Word {
+        val usedWords = mutableListOf<Word>()
+        if (wordList.size == usedWords.size) {
+            usedWords.clear()
         }
+
+        var word: Word? = null
+
+        do {
+            val rnd = (0 until wordList.size).random()
+            word = wordList[rnd]
+        } while (usedWords.contains(word))
+
+        usedWords.add(word!!)
+
+        return word
     }
 
-    fun loadAllWords() : Deferred<List<Word>> =
+
+    fun loadAllWords(): Deferred<List<Word>> =
         async(Dispatchers.IO) {
             db.wordDao().getAllWord()
         }
@@ -93,20 +81,10 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
         wordView.text = currentWord?.english
     }
 
-
     fun showNewWord() {
-
-        currentWord = wordList.getNewWord()
+        currentWord = getNewWord()
         wordView.text = currentWord?.swedish
     }
-
-    fun delete(word : Word) =
-        launch(Dispatchers.IO) {
-            db.wordDao().delete(word)
-        }
-
-
-
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
 
@@ -117,13 +95,27 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
         return true
     }
 
-
-
-
-
-
-
-
-
-
 }
+/*
+    fun showNewWord()  {
+        currentWord = wordList[4]
+       // wordView.text = currentWord?.english
+        // val rnd = (0 until wordList.size).random()
+      //  currentWord = wordList[rnd] // Random word
+        // currentWord = wordList.getNewWord()
+        wordView.setText(currentWord?.swedish)
+      /*= currentWord?.swedish */
+        // return wordList[rnd]
+/*
+        for (x in 0 until wordList.size) {
+            wordView.setText(result.get(x))
+        } */
+
+    } */
+
+/*
+    fun delete(word : Word) =
+        launch(Dispatchers.IO) {
+            db.wordDao().delete(word)
+        }
+*/
--- app/src/main/java/com/example/au22_flashcard/WordList.kt
+++ app/src/main/java/com/example/au22_flashcard/WordList.kt
@@ -1,5 +1,63 @@
 package com.example.au22_flashcard
 
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
+
+class WordList: AppCompatActivity(), CoroutineScope {
+
+    private lateinit var job: Job
+    private lateinit var db: AppDatabase
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
+    val wordList = mutableListOf<Word>()
+    private val usedWords = mutableListOf<Word>()
+    //  val defWordlist = loadAllWords()
+    //  launch {
+    //      val list = defWordlist.await()
+    //      for(word in list){
+    //          wordList.add(word)
+    //      }
+    //      showNewWord()
+//
+    //  }
+    // job = Job()
+//
+    // db = Room.databaseBuilder(applicationContext,
+    // AppDatabase::class.java,
+    // "word_items")
+    // .fallbackToDestructiveMigration()
+    // .build()
+
+
+    //alternativ 1
+    fun getNewWord(): Word {
+        if (wordList.size == usedWords.size) {
+            usedWords.clear()
+        }
+
+        var word: Word? = null
+
+        do {
+            val rnd = (0 until wordList.size).random()
+            word = wordList[rnd]
+        } while (usedWords.contains(word))
+
+        usedWords.add(word!!)
+
+        return word
+    }
+
+    fun loadAllWords(): Deferred<List<Word>> =
+        async(Dispatchers.IO) {
+            db.wordDao().getAllWord()
+        }
+}
+
+
+/* package com.example.au22_flashcard
+
 class WordList() {
     private val wordList = mutableListOf<Word>()
     private val usedWords = mutableListOf<Word>()
@@ -38,6 +96,7 @@ class WordList() {
 //    }
 
     //alternativ 1
+
     fun getNewWord() : Word {
         if (wordList.size == usedWords.size) {
             usedWords.clear()
@@ -60,7 +119,7 @@ class WordList() {
     // 3. använt ord tas bort från listan
     // 4. ordet håller reda på om det redan är använt
 
-}
+} */
 
 
 
